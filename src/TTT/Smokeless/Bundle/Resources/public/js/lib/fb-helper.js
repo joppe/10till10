2872139deea2fbc4179ad870ec5c8076c316d53c
Tim van den Bosch <tim@afriendofmine.nl>
2013-01-26 17:28:24 +0100

    share fb added

--- src/TTT/Smokeless/Bundle/Resources/public/js/lib/fb-helper.js
+++ src/TTT/Smokeless/Bundle/Resources/public/js/lib/fb-helper.js
@@ -2,10 +2,6 @@ fbHelper = {
 
 	/**
 	 * fb login
-	 *
-	 * @param string, permissions
-	 * @param string, fields
-	 * @param function, callback
 	 */
 	login: function (permissions, fields, callback) {
 		FB.login(function(response) {
@@ -48,6 +44,38 @@ fbHelper = {
 				}
 			}
 		);
+	}
+
+	/**
+	 * FB share
+	 */
+	share = function ($el, callback, options) {
+
+		// default options
+		var _options = {
+			method:			'feed',
+			name:			$el.data('fb_share_name'),
+			description:	$el.data('fb_share_description'),
+			link:			$el.data('fb_share_link'),
+			caption:		$el.data('fb_share_caption'),
+			picture:		$el.data('fb_share_picture')
+		};
+
+		// check for options
+		if (typeof options == 'object') {
+			for (var property in options) {
+				_options.property = options.property;
+			}
+		}
+	 
+	 	// share popup
+		FB.ui(_options, function(response) {
+			if (response && response.post_id) {
+				if (typeof callback == 'function') callback(response);
+			} else {
+				return false;
+			}
+		});
 	}
 
 };
