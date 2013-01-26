77ff6bdce3e9f5543b79b563eb8c742d4439a9bc
drm <drm@melp.nl>
2013-01-26 13:17:04 +0100

    routing setup

---
+++ src/TTT/Smokeless/Bundle/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Gerard van Helden <drm@melp.nl>
+ * @copyright 2013 Gerard van Helden
+ *
+ * For Licensing information, see the LICENSE file included with this project.
+ */
+namespace TTT\Smokeless\Bundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class IndexController
+{
+    /**
+     * @Template
+     * @Route("/")
+     */
+    function indexAction() {
+        return arraY();
+    }
+}
